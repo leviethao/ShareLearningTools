5f90d29fd0d8f8e132a0338fb7b446a9d2916ef3
leviethao <leviethao2@gmail.com>
2018-11-25 12:32:30 +0700

    config server host, modify routes prefix to deploy vps

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // serverHost: 'http://35.231.228.65/'
-  serverHost: 'http://localhost:8081/',
+  serverHost: 'http://35.231.228.65/server',
+  // serverHost: 'http://localhost:8081/',
   searchServiceHost: 'http://localhost:3000/'
 }
