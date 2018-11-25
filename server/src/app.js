b8c6aa0bdb11d5cc0879aa862f9c193abaee90ea
leviethao <leviethao2@gmail.com>
2018-11-25 14:03:43 +0700

    change database url to mlab

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 // connect mongodb database
 const mongoose = require('mongoose')
-// var mongodbUrl = 'mongodb://admin1:admin1@ds239638.mlab.com:39638/nckhdb'
-var mongodbUrl = 'mongodb://localhost:27017/sharelearningtools'
+var mongodbUrl = 'mongodb://admin1:admin1@ds239638.mlab.com:39638/nckhdb'
+// var mongodbUrl = 'mongodb://localhost:27017/sharelearningtools'
 mongoose.connect(mongodbUrl, {useNewUrlParser: true})
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
